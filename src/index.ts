a55626c58ee6fad4c2720143370a92eb81dfa529
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-14 08:44:30 +0900

    Add compile and render functions

--- src/index.ts
+++ src/index.ts
@@ -26,12 +26,27 @@ interface CompileOptions {
     renamer: (filename: string) => string;
 }
 
+interface CompileFunc {
+    (options?: Partial<CompileOptions> & pug.Options): Metalsmith.Plugin;
+    defaultOptions: CompileOptions;
+}
+
 interface RenderOptions {
     locals: pug.LocalsObject;
     useMetadata: boolean;
 }
 
+interface RenderFunc {
+    (options?: Partial<RenderOptions>): Metalsmith.Plugin;
+    defaultOptions: RenderOptions;
+}
+
 interface ConvertOptions extends CompileOptions, RenderOptions {}
+
+interface ConvertFunc {
+    (options?: Partial<ConvertOptions> & pug.Options): Metalsmith.Plugin;
+    defaultOptions: ConvertOptions;
+}
 
 function addFile(
     files: Metalsmith.Files,
@@ -46,7 +61,125 @@ function addFile(
     return newFile;
 }
 
-async function render(
+const compileTemplateMap: WeakMap<Buffer, pug.compileTemplate> = new WeakMap();
+
+async function compileEach(
+    filename: string,
+    files: Metalsmith.Files,
+    metalsmith: Metalsmith,
+    options: CompileOptions & pug.Options,
+): Promise<void> {
+    const data: unknown = files[filename];
+    if (!isFile(data)) {
+        return;
+    }
+
+    if (!isUtf8(data.contents)) {
+        return;
+    }
+
+    const {
+        pattern, // eslint-disable-line no-unused-vars, @typescript-eslint/no-unused-vars
+        renamer,
+        ...otherOpts
+    } = options;
+
+    const pugOptions: pug.Options = {
+        ...otherOpts,
+        filename: metalsmith.path(metalsmith.source(), filename),
+    };
+
+    const compileTemplate = pug.compile(data.contents.toString(), pugOptions);
+
+    const newFilename: string = renamer(filename);
+    const newFile = addFile(files, newFilename, '');
+    compileTemplateMap.set(newFile.contents, compileTemplate);
+
+    if (filename !== newFilename) {
+        delete files[filename];
+    }
+}
+
+const compileDefaultOptions: CompileOptions = {
+    pattern: ['**/*.pug'],
+    renamer: filename => filename.replace(/\.(?:pug|jade)$/, '.html'),
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const compile: CompileFunc = function(opts = {}) {
+    const options = {
+        ...compileDefaultOptions,
+        ...opts,
+    };
+
+    return (files, metalsmith, done) => {
+        const matchedFiles: string[] = match(
+            Object.keys(files),
+            options.pattern,
+        );
+
+        Promise.all(
+            matchedFiles.map(filename =>
+                compileEach(filename, files, metalsmith, options),
+            ),
+        )
+            .then(() => done(null, files, metalsmith))
+            .catch(error => done(error, files, metalsmith));
+    };
+};
+
+compile.defaultOptions = { ...compileDefaultOptions };
+
+async function renderEach(
+    filename: string,
+    files: Metalsmith.Files,
+    metalsmith: Metalsmith,
+    options: RenderOptions,
+): Promise<void> {
+    const data: unknown = files[filename];
+    if (!isFile(data)) {
+        return;
+    }
+
+    const { locals, useMetadata } = options;
+
+    if (useMetadata) {
+        Object.assign(locals, metalsmith.metadata(), data);
+    }
+
+    const compileTemplate = compileTemplateMap.get(data.contents);
+    if (compileTemplate) {
+        const convertedText: string = compileTemplate(locals);
+        data.contents = Buffer.from(convertedText, 'utf8');
+    }
+}
+
+const renderDefaultOptions: RenderOptions = {
+    locals: {},
+    useMetadata: false,
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const render: RenderFunc = function(opts = {}) {
+    const options = {
+        ...renderDefaultOptions,
+        ...opts,
+    };
+
+    return (files, metalsmith, done) => {
+        Promise.all(
+            Object.keys(files).map(filename =>
+                renderEach(filename, files, metalsmith, options),
+            ),
+        )
+            .then(() => done(null, files, metalsmith))
+            .catch(error => done(error, files, metalsmith));
+    };
+};
+
+render.defaultOptions = { ...renderDefaultOptions };
+
+async function convertEach(
     filename: string,
     files: Metalsmith.Files,
     metalsmith: Metalsmith,
@@ -92,16 +225,13 @@ async function render(
 }
 
 const convertDefaultOptions: ConvertOptions = {
-    pattern: ['**/*.pug'],
-    renamer: filename => filename.replace(/\.(?:pug|jade)$/, '.html'),
-    locals: {},
-    useMetadata: false,
+    ...compileDefaultOptions,
+    ...renderDefaultOptions,
 };
 
-export default function convert(
-    opts: Partial<ConvertOptions> & pug.Options = {},
-): Metalsmith.Plugin {
-    const options: ConvertOptions & pug.Options = {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const convert: ConvertFunc = function(opts = {}) {
+    const options = {
         ...convertDefaultOptions,
         ...opts,
     };
@@ -114,12 +244,15 @@ export default function convert(
 
         Promise.all(
             matchedFiles.map(filename =>
-                render(filename, files, metalsmith, options),
+                convertEach(filename, files, metalsmith, options),
             ),
         )
             .then(() => done(null, files, metalsmith))
             .catch(error => done(error, files, metalsmith));
     };
-}
+};
 
 convert.defaultOptions = { ...convertDefaultOptions };
+
+export { compile, render };
+export default convert;
