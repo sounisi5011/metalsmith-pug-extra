9d0e0392c667e453ff6107b030303ec418849294
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-14 20:53:36 +0900

    Add a test to examine the defaultOptions property of the compile() and render() functions

--- test/defaultOptions.ts
+++ test/defaultOptions.ts
@@ -1,63 +1,70 @@
 import test from 'ava';
-import pugConvert from '../src';
+import pugConvert, { compile, render } from '../src';
 
-const methodMap = new Map([['convert()', pugConvert]]);
+const methodMap = new Map<string, pugConvert | compile | render>([
+    ['convert()', pugConvert],
+    ['compile()', compile],
+    ['render()', render],
+]);
 
-test('methods have defaultOptions property', t => {
-    methodMap.forEach((value, key) => {
-        t.is(typeof value.defaultOptions, 'object', key);
+const compileMethodMap = new Map<string, pugConvert | compile>([
+    ['convert()', pugConvert],
+    ['compile()', compile],
+]);
+
+methodMap.forEach((value, key) => {
+    test(`${key} methods have defaultOptions property`, t => {
+        t.is(typeof value.defaultOptions, 'object');
     });
 });
 
-test('defaultOptions.pattern', t => {
-    const validDefaultPattern = ['**/*.pug'];
-
-    methodMap.forEach((value, key) => {
-        t.deepEqual(validDefaultPattern, value.defaultOptions.pattern, key);
+compileMethodMap.forEach((value, key) => {
+    test(`${key}: defaultOptions.pattern`, t => {
+        const validDefaultPattern = ['**/*.pug'];
+        t.deepEqual(validDefaultPattern, value.defaultOptions.pattern);
     });
 });
 
-test('defaultOptions.renamer', t => {
-    const validReplaceList = [
-        ['index.pug', 'index.html'],
-        ['basename.pug', 'basename.html'],
-        ['.pug', '.html'],
-        ['basename.html.pug', 'basename.html.html'],
-        ['basename.pug.pug', 'basename.pug.html'],
-        ['basename.pug.html', 'basename.pug.html'],
-        ['basename.pug.', 'basename.pug.'],
+const validReplaceList = [
+    ['index.pug', 'index.html'],
+    ['basename.pug', 'basename.html'],
+    ['.pug', '.html'],
+    ['basename.html.pug', 'basename.html.html'],
+    ['basename.pug.pug', 'basename.pug.html'],
+    ['basename.pug.html', 'basename.pug.html'],
+    ['basename.pug.', 'basename.pug.'],
 
-        ['index.jade', 'index.html'],
-        ['basename.jade', 'basename.html'],
-        ['.jade', '.html'],
-        ['basename.html.jade', 'basename.html.html'],
-        ['basename.jade.jade', 'basename.jade.html'],
-        ['basename.jade.html', 'basename.jade.html'],
-        ['basename.jade.', 'basename.jade.'],
+    ['index.jade', 'index.html'],
+    ['basename.jade', 'basename.html'],
+    ['.jade', '.html'],
+    ['basename.html.jade', 'basename.html.html'],
+    ['basename.jade.jade', 'basename.jade.html'],
+    ['basename.jade.html', 'basename.jade.html'],
+    ['basename.jade.', 'basename.jade.'],
 
-        ['dirname/index.pug', 'dirname/index.html'],
-        ['dirname/basename.pug', 'dirname/basename.html'],
-        ['dirname/.pug', 'dirname/.html'],
-        ['dirname/basename.html.pug', 'dirname/basename.html.html'],
-        ['dirname/basename.pug.pug', 'dirname/basename.pug.html'],
-        ['dirname/basename.pug.html', 'dirname/basename.pug.html'],
-        ['dirname/basename.pug.', 'dirname/basename.pug.'],
+    ['dirname/index.pug', 'dirname/index.html'],
+    ['dirname/basename.pug', 'dirname/basename.html'],
+    ['dirname/.pug', 'dirname/.html'],
+    ['dirname/basename.html.pug', 'dirname/basename.html.html'],
+    ['dirname/basename.pug.pug', 'dirname/basename.pug.html'],
+    ['dirname/basename.pug.html', 'dirname/basename.pug.html'],
+    ['dirname/basename.pug.', 'dirname/basename.pug.'],
 
-        ['/index.pug', '/index.html'],
-        ['/basename.pug', '/basename.html'],
-        ['/.pug', '/.html'],
-        ['/basename.html.pug', '/basename.html.html'],
-        ['/basename.pug.pug', '/basename.pug.html'],
-        ['/basename.pug.html', '/basename.pug.html'],
-        ['/basename.pug.', '/basename.pug.'],
-    ];
-
-    validReplaceList.forEach(([filename, newFilename]) => {
-        methodMap.forEach((value, methodName) => {
+    ['/index.pug', '/index.html'],
+    ['/basename.pug', '/basename.html'],
+    ['/.pug', '/.html'],
+    ['/basename.html.pug', '/basename.html.html'],
+    ['/basename.pug.pug', '/basename.pug.html'],
+    ['/basename.pug.html', '/basename.pug.html'],
+    ['/basename.pug.', '/basename.pug.'],
+];
+compileMethodMap.forEach((value, methodName) => {
+    test(`${methodName}: defaultOptions.renamer`, t => {
+        validReplaceList.forEach(([filename, newFilename]) => {
             t.is(
                 newFilename,
                 value.defaultOptions.renamer(filename),
-                `${methodName}: ${filename} -> ${newFilename}`,
+                `${filename} -> ${newFilename}`,
             );
         });
     });
