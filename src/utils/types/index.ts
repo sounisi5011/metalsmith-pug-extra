99f55b36ced63cb8ae27e4dbb275bbd326563423
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-06-28 06:31:20 +0900

    Add a comment to description the isReadonlyOrWritableArray type

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Modify the Array.isArray function so that it can correctly Type Guard the ReadonlyArray type.
+ * @example
+ *   (Array.isArray as isReadonlyOrWritableArray)(value)
+ *   (<isReadonlyOrWritableArray>Array.isArray)(value)
+ */
 export type isReadonlyOrWritableArray = (
     value: unknown,
 ) => value is unknown[] | readonly unknown[];
